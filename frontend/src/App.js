c6671172f10de978d0e48881e1cff5be039398b7
adumit <andrew.dumit@gmail.com>
2018-01-16 00:06:52 -0600

    Added react-grid-system and gridified the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CombatantColumn from './components/combatantColumn'
+import { Row, Col, Container } from 'react-grid-system';
+
 
 class App extends Component {
   render() {
@@ -11,16 +13,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div className="container">
-          <div className="row-fluid">
-            <div className="col-sm col-md col-lg">
-              <p>BLAH BLAH</p>
-            </div>
-            <div className="col-sm col-md col-lg">
-              <p>BLAH BLAH</p>
-            </div>
-          </div>
-        </div>
+        <Container>
+          <Row>
+            <Col>
+              <CombatantColumn/>
+            </Col>
+            <Col>
+              <CombatantColumn/>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
