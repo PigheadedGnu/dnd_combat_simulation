daf71c951f2929343992a5d1a26f832323642259
adumit <andrew.dumit@gmail.com>
2018-01-18 16:25:06 -0600

    Change index to use the app const

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './reducers/combatantSelection'
 import { Provider } from 'react-redux'
-import { combineReducers, createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
