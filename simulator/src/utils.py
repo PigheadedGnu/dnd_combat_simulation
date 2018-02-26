c864c29a608f7d59a9053bde91fef5fc9ebf731a
adumit <andrew.dumit@gmail.com>
2018-02-25 20:46:19 -0600

    Wrote script to parse source_monsters.json and committing source monsters

--- simulator/src/utils.py
+++ simulator/src/utils.py
@@ -1,6 +1,21 @@
 from random import randint
 from core.settings import BASE_DIR
 import json
+import math
+
+
+def convert_stat_to_bonus(stat_value):
+    """ Converts a real-valued stat to the bonus that it represents
+
+    Example: 18 strength -> +4
+
+    Args:
+        stat_value: the integer denoting the value of the stat to be converted
+
+    Returns:
+        stat_bonus: The bonus that the statistic value represents
+    """
+    return math.floor((stat_value - 10)/2)
 
 
 def calc_roll(dice):
